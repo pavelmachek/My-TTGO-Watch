658ded234ebc0f7ab63d17228de6c28e492950cd
Pavel Machek <pavel@ucw.cz>
2022-01-05 20:57:45 +0100

    Flushes are needed to get debug output. Wow.

--- src/app/example_app/example_app_main.cpp
+++ src/app/example_app/example_app_main.cpp
@@ -61,18 +61,18 @@ lv_obj_t *test_label = NULL;
 lv_obj_t *big_btn = NULL;
 
 bool example_button_cb( EventBits_t event, void *arg ) {
-  printf("button callback\n");
+  printf("button callback\n"); fflush(stdout);
     switch( event ) {
-    case BUTTON_LEFT:   printf("left button\n");
+    case BUTTON_LEFT:   printf("left button\n"); fflush(stdout);
                             break;
-    case BUTTON_RIGHT:  printf("right button\n");
+    case BUTTON_RIGHT:  printf("right button\n"); fflush(stdout);
                             break;
     }
     return( true );
 }
 
 bool example_app_touch_event_cb( EventBits_t event, void *arg ) {
-  printf("touch cb %d\n", event);
+  printf("touch cb %d\n", event); fflush(stdout);
     switch( event ) {
         case( TOUCH_UPDATE ):
             break;
@@ -81,13 +81,22 @@ bool example_app_touch_event_cb( EventBits_t event, void *arg ) {
 }
 
 static void exit_big_app_tile_event_cb( lv_obj_t * obj, lv_event_t event ) {
-  printf("big_btn\n");  
     switch( event ) {
-        case( LV_EVENT_SHORT_CLICKED ): 
+        case( LV_EVENT_SHORT_CLICKED ):
+	    printf("big_btn -- short\n"); fflush(stdout);
                                         break;
-        case( LV_EVENT_LONG_PRESSED ):  
+        case( LV_EVENT_LONG_PRESSED ):
+	    printf("big_btn -- long\n"); fflush(stdout);	  
                                         break;
     }    
+}
+
+void example_activate_cb( void ) {
+  printf("activate\n"); fflush(stdout);
+}
+
+void example_hibernate_cb( void ) {
+  printf("hibernate\n"); fflush(stdout);
 }
 
 void example_app_main_setup( uint32_t tile_num ) {
@@ -115,16 +124,19 @@ void example_app_main_setup( uint32_t tile_num ) {
     lv_obj_add_protect( big_btn, LV_PROTECT_CLICK_FOCUS );
     lv_obj_add_style( big_btn, LV_OBJ_PART_MAIN, &example_app_main_style );
     lv_obj_align( big_btn, example_app_main_tile, LV_ALIGN_CENTER, 0, 0 );
-#if 0
+
+#if 1
     lv_obj_set_event_cb( big_btn, exit_big_app_tile_event_cb );
 
     // FIXME: should use _activate_cb
     _example_app_task = lv_task_create( example_app_task, 5000, LV_TASK_PRIO_MID, NULL );
 #endif
-#if 0
+
+    mainbar_add_tile_activate_cb( tile_num, example_activate_cb );
+    mainbar_add_tile_hibernate_cb( tile_num, example_hibernate_cb );
     mainbar_add_tile_button_cb( tile_num, example_button_cb );
-    touch_register_cb( TOUCH_UPDATE , example_app_touch_event_cb, "osm touch" );
-#endif
+    
+    //    touch_register_cb( TOUCH_UPDATE , example_app_touch_event_cb, "osm touch" );
 }
 
 static void enter_example_app_setup_event_cb( lv_obj_t * obj, lv_event_t event ) {
@@ -157,11 +169,11 @@ void fetch_url() {
   //char url[] = "https://tgftp.nws.noaa.gov/data/observations/metar/decoded/LKPR.TXT";
     char url[] = "https://tgftp.nws.noaa.gov/data/observations/metar/stations/LKPR.TXT";
 
-    printf("Loading...\n");
+    printf("Loading...\n"); fflush(stdout);
     
     uri_load_dsc_t *uri_load_dsc = uri_load_to_ram( url );
 
-    printf("Got it... %d bytes, %s\n", uri_load_dsc->size, uri_load_dsc->data);
+    printf("Got it... %d bytes, %s\n", uri_load_dsc->size, uri_load_dsc->data); fflush(stdout);
 #define SIZE 1024
     char data[SIZE];
     int s = uri_load_dsc->size;
