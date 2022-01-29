7779b02e020351d651172757ac97e603fc72971f
Pavel Machek <pavel@ucw.cz>
2022-01-29 13:56:12 +0100

    Python experiments.

--- src/app/example_app/lua.cpp
+++ src/app/example_app/lua.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+#if 0
 extern "C" {
 #if 0
-#include "../../lua-on-arduino/src/lua/lua.h"
-#include "../../lua-on-arduino/src/lua/lualib.h"
-#include "../../lua-on-arduino/src/lua/lauxlib.h"
+#include "lua-on-arduino/src/lua/lua.h"
+#include "lua-on-arduino/src/lua/lualib.h"
+#include "lua-on-arduino/src/lua/lauxlib.h"
 #else
 #include <lua5.3/lua.h>
 #include <lua5.3/lualib.h>
@@ -74,3 +75,36 @@ void load (char *filename, int *width, int *height) {
   lua_close(L);
 }
 #endif
+#endif
+
+//#include "mbed.h"
+extern "C" {
+#include "runtime.h"
+
+#include "micropython/py/runtime.h"
+#include "micropython/py/mphal.h"
+#include "micropython/lib/utils/pyexec.h"
+}
+
+
+// Serial communication to host PC via USB
+Serial pc(USBTX, USBRX);
+
+// Implement the micropython HAL I/O functions
+extern "C" void mp_hal_stdout_tx_chr(int c);
+void mp_hal_stdout_tx_chr(int c) {
+    pc.putc(c);
+}
+extern "C" int mp_hal_stdin_rx_chr(void);
+int mp_hal_stdin_rx_chr(void) {
+    int c = pc.getc();
+    return c;
+}
+
+// Now the main program - run the REPL.
+int main() {
+    mp_init();
+    pyexec_friendly_repl();
+    mp_deinit();
+    return 0;
+}
