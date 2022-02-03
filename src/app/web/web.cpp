60327c50228d49e698dc18b5a63fc722c3d3ea47
Pavel Machek <pavel@ucw.cz>
2022-02-04 00:06:25 +0100

    Finish move from example_app to web.

--- src/app/web/web.cpp
+++ src/app/web/web.cpp
@@ -43,7 +43,7 @@ uint32_t web_setup_tile_num;
 icon_t *web = NULL;
 
 // widget icon
-icon_t *example_widget = NULL;
+icon_t *web_widget = NULL;
 
 // declare you images or fonts you need
 LV_IMG_DECLARE(web_64px);
@@ -51,36 +51,36 @@ LV_IMG_DECLARE(info_1_16px);
 
 // declare callback functions for the app and widget icon to enter the app
 static void enter_web_event_cb( lv_obj_t * obj, lv_event_t event );
-static void enter_example_widget_event_cb( lv_obj_t * obj, lv_event_t event );
+static void enter_web_widget_event_cb( lv_obj_t * obj, lv_event_t event );
 
 /*
- * setup routine for example app
+ * setup routine for web app
  */
 void web_setup( void ) {
     #if defined( ONLY_ESSENTIAL )
         return;
     #endif
     // register 2 vertical tiles and get the first tile number and save it for later use
-    web_main_tile_num = mainbar_add_app_tile( 1, 1, "example app" );
-    web_setup_tile_num = mainbar_add_setup_tile( 1, 1, "example app setup" );
+    web_main_tile_num = mainbar_add_app_tile( 1, 1, "web" );
+    web_setup_tile_num = mainbar_add_setup_tile( 1, 1, "web setup" );
 
     // register app icon on the app tile
     // set your own icon and register her callback to activate by an click
     // remember, an app icon must have an size of 64x64 pixel with an alpha channel
     // use https://lvgl.io/tools/imageconverter to convert your images and set "true color with alpha" to get fancy images
-    // the resulting c-file can put in /app/examples/images/ and declare it like LV_IMG_DECLARE( your_icon );
+    // the resulting c-file can put in /app/webs/images/ and declare it like LV_IMG_DECLARE( your_icon );
     web = app_register( "myapp", &web_64px, enter_web_event_cb );
     app_set_indicator( web, ICON_INDICATOR_OK );
 
-#ifdef EXAMPLE_WIDGET
+#ifdef WEB_WIDGET
     // register widget icon on the main tile
     // set your own icon and register her callback to activate by an click
     // remember, an widget icon must have an max size of 64x64 pixel
     // use https://lvgl.io/tools/imageconverter to convert your images and set "true color with alpha" to get fancy images
-    // the resulting c-file can put in /app/examples/images/ and declare it like LV_IMG_DECLARE( your_icon );
-    example_widget = widget_register( "myapp", &web_64px, enter_example_widget_event_cb );
-    widget_set_indicator( example_widget, ICON_INDICATOR_UPDATE );
-#endif // EXAMPLE_WIDGET
+    // the resulting c-file can put in /app/webs/images/ and declare it like LV_IMG_DECLARE( your_icon );
+    web_widget = widget_register( "myapp", &web_64px, enter_web_widget_event_cb );
+    widget_set_indicator( web_widget, ICON_INDICATOR_UPDATE );
+#endif // WEB_WIDGET
 
     // init main and setup tile, see web_main.cpp and web_setup.cpp
     web_main_setup( web_main_tile_num );
@@ -116,10 +116,10 @@ static void enter_web_event_cb( lv_obj_t * obj, lv_event_t event ) {
 /*
  *
  */
-static void enter_example_widget_event_cb( lv_obj_t * obj, lv_event_t event ) {
+static void enter_web_widget_event_cb( lv_obj_t * obj, lv_event_t event ) {
     switch( event ) {
         case( LV_EVENT_CLICKED ):       statusbar_hide( true );
-                                        widget_hide_indicator( example_widget );
+                                        widget_hide_indicator( web_widget );
                                         mainbar_jump_to_tilenumber( web_main_tile_num, LV_ANIM_OFF );
                                         break;
     }    
