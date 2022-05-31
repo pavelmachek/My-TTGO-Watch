196f04f69feff1a594ff54ecaabe89818372b85d
Pavel Machek <pavel@ucw.cz>
2022-05-31 22:27:49 +0200

    lua: Test that it works.

--- src/app/web/lua.cpp
+++ src/app/web/lua.cpp
@@ -23,6 +23,14 @@ static void lua_add_callbacks(lua_State *L)
   lua_setglobal(L, "c_getversion");
 }
 
+static char lua_code[] =
+  "width = c_getversion(0)\n"
+  "height = c_getversion(-42)\n"
+  "function say_hello(a)\n"
+  "  return 'Saying hello C got parameter '..a\n"
+  "end\n"
+  "text = 'hello C'\n";
+
 void lua_test(void) {
   printf("Should test lua\n");
   
@@ -40,7 +48,7 @@ void lua_test(void) {
     printf("cannot run configuration file: %s",
 	  lua_tostring(L, -1));
 #else
-  if (luaL_loadstring(L, "width = c_getversion(0); height = c_getversion(-42); text = 'hello C'") || lua_pcall(L, 0, 0, 0))
+  if (luaL_loadstring(L, lua_code) || lua_pcall(L, 0, 0, 0))
     printf("cannot run configuration file: %s",
 	  lua_tostring(L, -1));
 #endif
@@ -58,7 +66,20 @@ void lua_test(void) {
   printf("Got text = %s\n", (char *)lua_tostring(L, -3));
   printf("Got width = %d\n", (int)lua_tonumber(L, -2));
   printf("Got height = %d\n", (int)lua_tonumber(L, -1));
+
+  lua_getglobal(L, "say_hello");  /* function to be called */
+  lua_pushstring(L, "argument from C");   /* push 1st argument */
+  if (lua_pcall(L, 1, 1, 0) != 0)
+        printf("error running function `f': %s",
+                 lua_tostring(L, -1));
     
+  /* retrieve result */
+  if (!lua_isstring(L, -1))
+    printf( "function `f' must return a number");
+  
+  printf("Got call result = %s\n", (char *)lua_tostring(L, -1));
+  lua_pop(L, 1);  /* pop returned value */
+  
   lua_close(L);
 
   printf("Done testing lua\n");
